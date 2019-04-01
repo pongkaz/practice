d7f0ae65d8b76ae845e4b61febbd61843b35ed9d
ponkaj sarker <pongkaz89@gmail.com>
2019-04-01 18:07:05 +0600

    Chnages my some code here

--- Hello.java
+++ Hello.java
@@ -1,7 +1,13 @@
 public class Hello{
    public static void main(String args[]){
        System.out.println("Hello World");
+         
+         greting("Hy i am ponkaj sarker");
 
+   }
+
+   public static void greting(String name){
+      System.out.println("Wellcome"+ name);
 
    }
 }
